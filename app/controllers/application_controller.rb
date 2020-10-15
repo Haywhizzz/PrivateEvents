6303f856f678358c6e45a8f4db8ef4828f348ca4
haywhizzz <haywhizzz@gmail.com>
2020-10-15 16:28:51 +0100

    Add attendees page for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
     def CheckUser
        if !session[:user_id]
        redirect_to new_session_path
        end
+    end
+    def current_user
+        @current_user = session[:user_id] if session[:user_id]
+        # @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def show
+        # @user = User.find(params[:id])
+        puts params
+    end
     
     def create
         @user = User.new(params_user)
