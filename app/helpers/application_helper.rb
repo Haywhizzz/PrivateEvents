827561233170d205e48f743f87a6f3bc20d1441b
Abiola Doyinsola <ifeobayemi@gmail.com>
2020-10-17 13:41:59 +0100

    Styled buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
   def show_sign_in_or_out
     out = ''
     if session[:user_id]
-      out << link_to('Sign Out', session_path(session[:user_id]), method: :delete)
+      out << link_to('Sign Out', session_path(session[:user_id]), method: :delete, class: "btn btn-danger")
 
     else
-      out << link_to('login', new_session_path, class: 'btn btn-outline-success mr-3')
-      out << link_to('Sign up', new_user_path, class: 'btn btn-outline-primary')
+      out << link_to('login', new_session_path, class: 'btn btn-success mr-3')
+      out << link_to('Sign up', new_user_path, class: 'btn btn-primary pt-2')
     end
 
     out.html_safe
