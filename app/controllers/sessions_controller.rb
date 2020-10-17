20e67e01be1a1df94034f6665931d187e45973ce
Abiola Doyinsola <ifeobayemi@gmail.com>
2020-10-17 15:51:47 +0100

    Clean up bugs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to new_user_path, danger: 'Successfully signed out'
+    redirect_to new_user_path
   end
 end
