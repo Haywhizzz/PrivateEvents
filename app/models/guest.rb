8df80bef30f203e90b631c01fedc22c11116d8aa
Abiola Doyinsola <ifeobayemi@gmail.com>
2020-10-19 13:33:16 +0100

    Update validation for registration

--- app/models/guest.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class Guest < ApplicationRecord
-  belongs_to :attendee, class_name: 'User'
-  belongs_to :attended_event, class_name: 'Event'
-end
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,4 +3,7 @@
 class Registration < ApplicationRecord
   belongs_to :attendee, class_name: 'User'
   belongs_to :attended_event, class_name: 'Event'
+
+  validates :attendee_id, presence: true, uniqueness: true
+
 end
